6b75731fbbb1af335a38c2b23c073390fbbb7b6a
minhoe kim <kminhoe10@gmail.com>
2023-02-03 12:41:00 +0900

    Roomie 0.1

---
+++ Roomie/src/main/java/roomie/common/test/testController.java
@@ -0,0 +1,42 @@
+package roomie.common.test;
+
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+public class testController {
+
+	@RequestMapping(value = "/test.da")
+	public ModelAndView updateForm(@RequestParam HashMap<String, Object> param) throws Exception {
+
+		ModelAndView mv = new ModelAndView("test");
+		
+		System.out.println(param);
+
+		
+		return mv;
+
+	}
+
+}
---
+++ Roomie/src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form action="/test.da">
+
+<input type="text" name="name1">
+
+<input type="text" name="name2">
+
+<input type="text" name="name3">
+
+<button>전송</button>
+
+
+
+</form>
+
+
+
+</body>
+</html>
