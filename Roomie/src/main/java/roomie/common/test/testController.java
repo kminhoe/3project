8f62bb587655ee051166b4d7809343fd1c311258
minhoe kim <kminhoe10@gmail.com>
2023-02-07 11:17:43 +0900

    Roomie 0.2

--- Roomie/src/main/java/roomie/common/test/testController.java
+++ Roomie/src/main/java/roomie/common/test/testController.java
@@ -1,5 +1,6 @@
 package roomie.common.test;
 
+import java.io.File;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,9 +18,12 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -28,11 +32,39 @@ import org.springframework.web.servlet.ModelAndView;
 public class testController {
 
 	@RequestMapping(value = "/test.da")
-	public ModelAndView updateForm(@RequestParam HashMap<String, Object> param) throws Exception {
+	public ModelAndView updateForm(@RequestParam HashMap<String, Object> param, HttpServletRequest request) throws Exception {
+
+		ModelAndView mv = new ModelAndView("test");
+
+		
+		return mv;
+
+	}
+	
+	@PostMapping(value = "/test1.da")
+	public ModelAndView updateForm2(@RequestParam("camera") MultipartFile file ,HttpServletRequest request) throws Exception {
 
 		ModelAndView mv = new ModelAndView("test");
 		
-		System.out.println(param);
+		//System.out.println(param);
+		
+		MultipartFile profUploadFile = file;
+		
+
+		String path = request.getSession().getServletContext().getRealPath("resources/img/");
+		
+		System.out.println("경로 주소 : " + path);
+		
+
+		if (!profUploadFile.isEmpty()) {
+			String logoFileName = profUploadFile.getOriginalFilename(); // 저장할 로고 이미지 파일명
+
+			profUploadFile.transferTo(new File(path + logoFileName)); // 업로드한 파일 저장
+			
+			System.out.println(">> 로고 이미지 업로드 파일 : " + logoFileName);
+		}
+		
+
 
 		
 		return mv;
--- Roomie/src/main/webapp/WEB-INF/views/test.jsp
+++ Roomie/src/main/webapp/WEB-INF/views/test.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-<form action="/test.da">
+<form action="/test1.da" method="post" enctype="multipart/form-data">
 
 <input type="text" name="name1">
 
@@ -16,12 +16,35 @@
 
 <input type="text" name="name3">
 
-<button>전송</button>
 
+<!-- <input type="file" accept="image/*" capture="camera" id="camera" name="camera"> -->
+
+<input type="file" id="camera" name="camera">
+<img id="frame">
+
+<button>전송</button>
 
 
 </form>
 
+<script>
+var camera = document.getElementById('camera');
+var frame = document.getElementById('frame');
+
+camera.addEventListener('change', function(e) {
+var file = e.target.file;
+
+if(camera != null){
+alert(camera.value);}
+else {
+	alert("카메라 없음");
+}
+
+frame.src = URL.createObjectURL(file);
+});
+
+
+</script>
 
 
 </body>
